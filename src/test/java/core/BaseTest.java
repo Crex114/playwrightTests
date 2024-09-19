6060a5e467b6115950ba4a8e615cc927faf141a3
Evgen <qwe@qwe.ru>
2024-09-19 19:31:06 +0300

    ref

--- src/test/java/core/BaseTest.java
+++ src/test/java/core/BaseTest.java
@@ -31,7 +31,7 @@ public class BaseTest {
 
         browser = Playwright
                 .create()
-                .firefox()
+                .chromium()
                 .launch(new BrowserType.LaunchOptions().setHeadless(false));//.setChannel("chrome"));
 
         context = browser.newContext();
